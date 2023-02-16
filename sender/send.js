06a0e20cb44b2bc4769eb51a837152ed29820b06
Julio Contreras <julio-alberto.aranis@soprasteria.com>
2023-02-16 02:11:03 +0100

    uploas works

--- sender/send.js
+++ sender/send.js
@@ -5,11 +5,11 @@ const FormData = require('form-data')
 // main
 ;(async () => {
   const data = new FormData()
-  data.append('destiny', 'user1/image.jpg')
+  data.append('fileName', 'image.jpg')
   data.append('file', fs.createReadStream('image.jpg'))
 
   await axios
-    .post('http://localhost:4010/api/media/upload', data, {
+    .post('http://localhost:4010/api/media/user/1', data, {
       headers: {
         accept: 'application/json',
         'Accept-Language': 'en-US,en;q=0.8',
